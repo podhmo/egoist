5cbae1eb2c5f71e2541e92f46d2015c643a8b1db
podhmo <ababjam61+github@gmail.com>
2020-04-22 06:13:56 +0900

    generate main.go

--- gogen/generate.py
+++ gogen/generate.py
@@ -1,13 +1,14 @@
 import typing as t
+import pathlib
 from prestring.output import output
-from .scan import scan_module, Command
+from .scan import Command
 from .naming import get_path_from_function_name
 
 
-def generate_all(fns: t.Dict[str, Command], *, root: str):
+def generate_all(fns: t.Dict[str, Command], *, root: str) -> None:
     with output(root=root) as fs:
         for name, fn in fns.items():
             fpath = get_path_from_function_name(fn.__name__)
-            with fs.open(fpath, "w") as wf:
-                wf.write("hai")
 
+            with fs.open(str(pathlib.Path(fpath) / "main.go"), "w") as wf:
+                wf.write("package main")
