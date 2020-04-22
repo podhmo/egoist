181fb24f906cf660a6f991a493d0a7ca510e8636
podhmo <ababjam61+github@gmail.com>
2020-04-22 13:11:41 +0900

    chore: init

--- examples/e2e/init/00hello/definitions.py
+++ examples/e2e/init/00hello/definitions.py
@@ -1,3 +1,33 @@
-def hello(*, name:str) -> None:
+from gogen import runtime
+
+
+def hello(*, name: str) -> None:
     """hello message"""
-    pass
+    from gogen.generate import cli
+
+    with runtime.generate(cli) as m:
+        runtime.printf("hello %s\n", name)
+        m.return_("nil")
+
+
+if __name__ == "__main__":
+    from gogen.cmdutil import as_subcommand, Config
+
+    @as_subcommand
+    def describe():
+        from gogen.cli import describe
+
+        describe(__name__)
+
+    @as_subcommand
+    def generate(*, root: str = "cmd"):
+        from gogen.generate import generate_all
+        import pathlib
+        from gogen.scan import scan_module
+        import sys
+
+        rootdir = pathlib.Path(__file__).parent / root
+        fns = scan_module(sys.modules[__name__])
+        generate_all(fns, root=rootdir)
+
+    as_subcommand.run(config=Config(ignore_expose=True))
