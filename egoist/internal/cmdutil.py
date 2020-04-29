3d115a5c52915364f96980b4e4847932dee52b43
podhmo <ababjam61+github@gmail.com>
2020-04-29 18:28:24 +0900

    Graph (#14)
    * feat: add graph module
    
    * feat: as command
    
    * chore: update examples

--- egoist/internal/cmdutil.py
+++ egoist/internal/cmdutil.py
@@ -1,4 +1,6 @@
-from handofcats import as_subcommand, Config
+from handofcats import as_command
+from handofcats import as_subcommand
+from handofcats import Config
 from handofcats import get_default_multi_driver
 
 
@@ -7,4 +9,4 @@ def is_marked_subcommand(fn):
     return fn in d.functions
 
 
-__all__ = ["as_subcommand", "Config"]
+__all__ = ["as_command", "as_subcommand", "Config"]
