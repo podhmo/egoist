50c4801d0a0eb6c24632fbbb792c43a03fa524b3
podhmo <ababjam61+github@gmail.com>
2020-04-30 19:46:56 +0900

    chore: add examples

---
+++ examples/e2e/generate/05use-rest-args/definitions.py
@@ -0,0 +1,16 @@
+from egoist import runtime
+
+
+def hello(*, name: str) -> None:
+    """hello message"""
+    from egoist.generate.clikit import clikit
+
+    with runtime.generate(clikit) as m:
+        args = runtime.get_cli_rest_args()
+        target = m.symbol("target")
+        with m.for_(f"_, {target} := range {args}"):
+            runtime.printf("%s: hello %s\n", name, target)
+
+
+if __name__ == "__main__":
+    runtime.main(name=__name__, here=__file__, root="cmd")
