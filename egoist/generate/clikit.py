5a998a2f23771bc17d195ff4ec8d9d5837f9900e
podhmo <ababjam61+github@gmail.com>
2020-05-01 09:46:13 +0900

    fix: fix generate.clikit

--- egoist/generate/clikit.py
+++ egoist/generate/clikit.py
@@ -1,12 +1,15 @@
+import typing as t
 import contextlib
 import inspect
 from egoist import runtime
-from egoist.internal.prestringutil import goname
+from egoist.internal.prestringutil import goname, Module
 from egoist.go.resolver import get_resolver, Resolver
 
 
 @contextlib.contextmanager
-def clikit(env: runtime.Env, *, resolver: Resolver = get_resolver()) -> None:
+def clikit(
+    env: runtime.Env, *, resolver: Resolver = get_resolver()
+) -> t.Iterator[Module]:
     m = env.m
     fn = env.fn
     spec = env.fnspec
