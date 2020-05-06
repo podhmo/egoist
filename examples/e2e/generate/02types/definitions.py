389577c619dd42256e171a0fa6635cde78052708
podhmo <ababjam61+github@gmail.com>
2020-05-07 00:07:01 +0900

    chore: update examples

--- examples/e2e/generate/02types/definitions.py
+++ examples/e2e/generate/02types/definitions.py
@@ -5,16 +5,14 @@ settings: SettingsDict = {"root": "cmd/", "here": __file__}
 app = App(settings)
 
 app.include("egoist.directives.define_cli")
-define_cli = app.define_cli("egoist.generate:walk")
 
 
-@define_cli
+@app.define_cli("egoist.generate.clikit:walk")
 def hello(
     *, name: str = "world", age: types.uint, debug: types.bool, dur: types.duration
 ) -> None:
     """hello message"""
-    from egoist import runtime
-    from egoist.generate.clikit import clikit
+    from egoist.generate.clikit import runtime, clikit
 
     with runtime.generate(clikit):
         runtime.printf("hello %s(%d)\n", name, age)
