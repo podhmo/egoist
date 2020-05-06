854ec701b5070ede17fbac97b7439adf0d34c39e
podhmo <ababjam61+github@gmail.com>
2020-05-06 18:50:28 +0900

    chore: update examples

--- examples/e2e/generate/00simple/definitions.py
+++ examples/e2e/generate/00simple/definitions.py
@@ -1,16 +1,26 @@
-from egoist import runtime
+from egoist.app import App, SettingsDict
+
+settings: SettingsDict = {"root": "cmd/", "here": __file__}
+app = App(settings)
+
+app.include("egoist.directives.define_cli")
+define_cli = app.define_cli("egoist.generate:walk")
 
 
+@define_cli
 def hello(*, name: str) -> None:
     """hello message"""
+    from egoist import runtime
     from egoist.generate.clikit import clikit
 
     with runtime.generate(clikit):
         runtime.printf("hello %s\n", name)
 
 
+@define_cli
 def byebye(*, name: str) -> None:
     """byebye message"""
+    from egoist import runtime
     from egoist.generate.clikit import clikit
 
     with runtime.generate(clikit):
@@ -18,4 +28,4 @@ def byebye(*, name: str) -> None:
 
 
 if __name__ == "__main__":
-    runtime.main(name=__name__, here=__file__, root="cmd")
+    app.run()
