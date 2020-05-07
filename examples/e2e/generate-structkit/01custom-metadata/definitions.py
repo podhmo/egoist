523b7eef1665b9ac03ca8266e31c1d5e04c9e9f6
podhmo <ababjam61+github@gmail.com>
2020-05-08 02:27:12 +0900

    chore: add examples

---
+++ examples/e2e/generate-structkit/01custom-metadata/definitions.py
@@ -0,0 +1,31 @@
+import typing as t
+from egoist.app import App, SettingsDict
+
+settings: SettingsDict = {"root": "", "here": __file__}
+app = App(settings)
+
+app.include("egoist.directives.define_struct_set")
+
+
+class Person:
+    name: str
+    age: int
+
+
+@app.define_struct_set("egoist.generate.structkit:walk")
+def models__models() -> None:
+    from egoist.generate.structkit import runtime, structkit
+
+    @runtime.set_metadata_handler
+    def metadata_handler(
+        cls: t.Type[t.Any], *, name: str, info: t.Any, metadata: runtime.Metadata
+    ) -> None:
+        """Yaml also added"""
+        metadata["tags"] = {"json": [name.rstrip("_")], "yaml": [name.rstrip("_")]}
+
+    with runtime.generate(structkit, classes=[Person]) as m:
+        m.package("models")
+
+
+if __name__ == "__main__":
+    app.run()
