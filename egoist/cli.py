cd9ce9aa6aeda05b61f3b863ddb97ec49ef0eeb9
podhmo <ababjam61+github@gmail.com>
2020-05-08 01:52:52 +0900

    Init struct (#42)
    * feat: egoist init structkit
    
    * chore: update examples
    
    * feat: logging message
    
    * hmm

--- egoist/cli.py
+++ egoist/cli.py
@@ -1,11 +1,14 @@
 import typing as t
 
 
-def init(*, root: str = ".") -> None:
+def init(*, target: str = "clikit", root: str = ".") -> None:
     """scaffold"""
+    import logging
     import pathlib
     import shutil
     from importlib.util import find_spec
+
+    logger = logging.getLogger(__name__)
 
     spec = find_spec("egoist")
     if spec is None:
@@ -15,8 +18,9 @@ def init(*, root: str = ".") -> None:
         return
     dirpath = pathlib.Path(locations[0]) / "data"
 
-    src = dirpath / "definitions.py.tmpl"
+    src = dirpath / f"{target}_definitions.py.tmpl"
     dst = pathlib.Path(root) / "definitions.py"
+    logger.info("create %s", dst)
     shutil.copy(src, dst)
 
 
@@ -40,6 +44,9 @@ def main(argv: t.Optional[t.List[str]] = None) -> t.Any:
         fn.__name__, help=fn.__doc__, formatter_class=parser.formatter_class
     )
     sub_parser.add_argument("--root", required=False, default=".", help="-")
+    sub_parser.add_argument(
+        "target", nargs="?", default="clikit", choices=["clikit", "structkit"]
+    )
     sub_parser.set_defaults(subcommand=fn)
 
     activate = logging_setup(parser)
