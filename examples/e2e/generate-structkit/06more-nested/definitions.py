a76652d3fc217d97be6270cb466308e868575f43
podhmo <ababjam61+github@gmail.com>
2020-05-09 04:37:27 +0900

    chore: add examples, more nested

---
+++ examples/e2e/generate-structkit/06more-nested/definitions.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+import typing as t
+from egoist.app import App, SettingsDict
+
+settings: SettingsDict = {"root": "", "here": __file__}
+app = App(settings)
+
+app.include("egoist.directives.define_struct_set")
+
+
+class Person:
+    name: str
+    age: int
+
+    followings: t.List[t.List[Person]]
+    followings2: t.List[t.List[t.Optional[Person]]]
+
+    groups: t.Dict[str, t.Dict[str, Person]]
+    groups2: t.Dict[str, t.Dict[str, t.Optional[Person]]]
+
+
+@app.define_struct_set("egoist.generators.structkit:walk")
+def models__models() -> None:
+    from egoist.generators.structkit import runtime, structkit
+
+    with runtime.generate(structkit, classes=[Person]) as m:
+        m.package("models")
+
+
+if __name__ == "__main__":
+    app.run()
