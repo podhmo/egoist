8e3b2268dd99d6e5cc4e7e8e679a788d42fa0bca
podhmo <ababjam61+github@gmail.com>
2020-05-14 05:23:58 +0900

    fix: type assertion

--- egoist/components/fs.py
+++ egoist/components/fs.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import typing as t
+import typing_extensions as tx
 import pathlib
 from egoist.app import App
 from egoist.runtime import get_components
@@ -10,11 +11,17 @@ if t.TYPE_CHECKING:
     from egoist.internal.prestringutil import Module, output
 
 
+class FSFactory(tx.Protocol):
+    def __call__(self, *, root: t.Union[str, pathlib.Path]) -> output[Module]:
+        ...
+
+
 def open_fs(*, root: t.Union[str, pathlib.Path]) -> output[Module]:
-    return get_components(NAME)  # type: ignore
+    factory = get_components(NAME)
+    return factory(root=root)  # type: ignore
 
 
-def create_fs(self: t.Any, *, root: t.Union[pathlib.Path, str]) -> output[Module]:
+def create_fs(*, root: t.Union[pathlib.Path, str]) -> output[Module]:
     """actual component"""
     from egoist.internal.prestringutil import output, Module
 
@@ -22,4 +29,5 @@ def create_fs(self: t.Any, *, root: t.Union[pathlib.Path, str]) -> output[Module
 
 
 def includeme(app: App) -> None:
-    app.register_component(NAME, create_fs)
+    actual: FSFactory = create_fs
+    app.register_component(NAME, actual)
