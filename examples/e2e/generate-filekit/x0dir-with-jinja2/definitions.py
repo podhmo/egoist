ec7fe6be081c241682ea3d18879fbb21b0cc076a
podhmo <ababjam61+github@gmail.com>
2020-05-17 08:28:13 +0900

    Use template examples (#84)
    * chore: use template
    
    * chore: jinja2 example
    
    * chore: use lru_cache(), but, in scan, undesired import is exised
    
    * chore: remove unused files

---
+++ examples/e2e/generate-filekit/x0dir-with-jinja2/definitions.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+import typing as t
+from functools import lru_cache
+from egoist.app import App, SettingsDict, parse_args
+
+if t.TYPE_CHECKING:
+    from jinja2.environment import Environment as Jinja2Environment
+
+settings: SettingsDict = {"rootdir": "", "here": __file__}
+app = App(settings)
+
+app.include("egoist.directives.define_dir")
+
+
+@lru_cache(1)
+def get_jinja2_environment() -> Jinja2Environment:
+    from jinja2 import Environment, FunctionLoader, StrictUndefined
+
+    env = Environment(
+        loader=FunctionLoader(lambda name: open(name).read()),
+        undefined=StrictUndefined,
+    )
+    return env
+
+
+@app.define_dir("egoist.generators.dirkit:walk")
+def output(
+    *, fizzbuzz_template="templates/fizzbuzz.j2", inputs_template="templates/inputs.j2",
+) -> None:
+    from egoist.generators.dirkit import runtime
+
+    env = get_jinja2_environment()
+    with runtime.create_file(f"fizzbuzz.txt", depends_on=[fizzbuzz_template]) as wf:
+        t = env.get_template(str(fizzbuzz_template))
+        print(t.render(n=30), file=wf)
+
+    with runtime.create_file(f"inputs.html", depends_on=[inputs_template]) as wf:
+        t = env.get_template(str(inputs_template))
+        print(t.render(), file=wf)
+
+
+if __name__ == "__main__":
+    for argv in parse_args(sep="-"):
+        app.run(argv)
