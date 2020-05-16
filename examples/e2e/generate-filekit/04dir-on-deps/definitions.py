c99aebdb1bbb6675e0c290127f6bc9519b96fc8c
podhmo <ababjam61+github@gmail.com>
2020-05-17 00:49:02 +0900

    chore: update examples

---
+++ examples/e2e/generate-filekit/04dir-on-deps/definitions.py
@@ -0,0 +1,34 @@
+from egoist.app import App, SettingsDict, parse_args
+
+settings: SettingsDict = {"rootdir": "", "here": __file__}
+app = App(settings)
+
+app.include("egoist.directives.define_dir")
+
+
+@app.define_dir("egoist.generators.dirkit:walk")
+def output(*, source="input/season.csv") -> None:
+    import csv
+    import json
+    from egoist.generators.dirkit import runtime
+
+    with runtime.create_file("seasons.json", depends_on=[source]) as wf:
+        name_list = []
+        ja_list = []
+
+        with open(source) as rf:
+            for row in csv.DictReader(rf):
+                name_list.append(row["name"])
+                ja_list.append(row["ja"])
+
+        data = {
+            "definitions": {
+                "season": {"type": "string", "enum": name_list, "x-ja-enum": ja_list}
+            }
+        }
+        json.dump(data, wf, indent=2, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    for argv in parse_args(sep="-"):
+        app.run(argv)
