195f06a522d614f498f08ecb18fc6bd65fbf33fc
podhmo <ababjam61+github@gmail.com>
2020-05-16 19:10:04 +0900

    chore: scan.output

--- examples/e2e/generate-filekit/build.mk
+++ examples/e2e/generate-filekit/build.mk
@@ -1,9 +1,12 @@
 default:
 	$(MAKE) clean
+	$(MAKE) scan
 	$(MAKE) generate
 
 generate:
 	python definitions.py generate
+scan:
+	python definitions.py scan | tee scan.output
 .PHONY: generate
 
 clean:
