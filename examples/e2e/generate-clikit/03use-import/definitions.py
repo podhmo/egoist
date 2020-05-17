5b9d01ac32ef88694f0a11e5047aacf0c45db794
podhmo <ababjam61+github@gmail.com>
2020-05-17 16:00:51 +0900

    Clikit shortname (#87)
    * chore: unexpected example
    
    * feat: use shortname
    
    * chore: update examples

--- examples/e2e/generate-clikit/03use-import/definitions.py
+++ examples/e2e/generate-clikit/03use-import/definitions.py
@@ -1,13 +1,13 @@
 from egoist.app import App, SettingsDict, parse_args
 
-settings: SettingsDict = {"rootdir": "cmd/", "here": __file__}
+settings: SettingsDict = {"rootdir": "", "here": __file__}
 app = App(settings)
 
 app.include("egoist.directives.define_cli")
 
 
 @app.define_cli("egoist.generators.clikit:walk")
-def hello(*, name: str = "World") -> None:
+def cmd__hello(*, name: str = "World") -> None:
     """hello message"""
     from egoist.generators.clikit import runtime, clikit
 
