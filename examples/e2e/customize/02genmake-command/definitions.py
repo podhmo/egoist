08c12dc126187df6eb5ea0854105292c54c7a885
podhmo <ababjam61+github@gmail.com>
2020-05-20 02:14:07 +0900

    chore: update example

--- examples/e2e/customize/02genmake-command/definitions.py
+++ examples/e2e/customize/02genmake-command/definitions.py
@@ -4,30 +4,41 @@ from egoist.app import create_app, SettingsDict, parse_args
 settings: SettingsDict = {"rootdir": "", "here": __file__}
 app = create_app(settings)
 
+
 app.include("commands.genmake")
 app.include("egoist.directives:define_dir")
+app.include("egoist.directives:define_file")
 
 
-@app.define_dir("egoist.generators.dirkit:walk")
-def output(
+@app.define_dir("egoist.generators.dirkit:walk", rename="output")
+def x(
     *,
     gen_py: str = "_tools/gen.py",
-    # for x
     x_yaml_ja2: str = "_tools/x-yaml.j2",
     x_go_ja2: str = "_tools/x-go.j2",
     x_csv: str = "input/x.csv",
-    # for y
-    y_go_ja2: str = "_tools/y-go.j2",
-    root_yaml: str = "input/root.yaml",
 ):
     from egoist.generators.dirkit import runtime
 
-    with runtime.create_file("x.yaml", depends_on=[gen_py, x_yaml_ja2, x_csv]) as wf:
-        print(_gen([gen_py, x_yaml_ja2, x_csv]), file=wf)
-    with runtime.create_file("x.go", depends_on=[gen_py, x_go_ja2, x_csv]) as wf:
-        print(_gen([gen_py, x_go_ja2, x_csv]), file=wf)
+    inputs = [gen_py, x_yaml_ja2, x_csv]
+    with runtime.create_file("x.yaml", depends_on=inputs) as wf:
+        print(_gen(inputs), file=wf)
 
-    with runtime.create_file("y.go", depends_on=[gen_py, y_go_ja2, root_yaml]) as wf:
+    inputs = [gen_py, x_go_ja2, x_csv]
+    with runtime.create_file("x.go", depends_on=inputs) as wf:
+        print(_gen(inputs), file=wf)
+
+
+@app.define_file("egoist.generators.filekit:walk", rename="output/y.go")
+def y_go(
+    *,
+    gen_py: str = "_tools/gen.py",
+    y_go_ja2: str = "_tools/y-go.j2",
+    root_yaml: str = "input/root.yaml",
+):
+    from egoist.generators.filekit import runtime
+
+    with runtime.create_file() as wf:
         print(_gen([gen_py, y_go_ja2, root_yaml]), file=wf)
 
 
