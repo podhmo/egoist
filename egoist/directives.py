210a3e3838ae2487f99df6c1dfb102b664116532
podhmo <ababjam61+github@gmail.com>
2020-05-23 22:39:40 +0900

    feat: use directive decorator

--- egoist/directives.py
+++ egoist/directives.py
@@ -1,8 +1,66 @@
 import typing as t
+from functools import partial, update_wrapper
 from .app import App, _noop
 
 AnyFunction = t.Callable[..., t.Any]
 T = t.TypeVar("T")
+
+
+class Directive:
+    def __init__(
+        self, define_fn: AnyFunction, *, name: str, requires: t.List[str]
+    ) -> None:
+        self.define_fn = define_fn
+        self.seen: bool = False
+        self.name = name
+        self.requires = requires
+
+    @property
+    def __call__(self) -> AnyFunction:
+        return self.define_fn
+
+    def register(self, app: App, *args, **kwargs) -> AnyFunction:
+        def _register(target_fn: AnyFunction) -> AnyFunction:
+            if not self.seen:
+                self.seen = True
+            self.define_fn(app, target_fn, *args, **kwargs)
+            return target_fn
+
+        return _register
+
+    def includeme(self, app: App) -> None:
+        """callback for app.include()"""
+        # for information used by describe()
+        directive = partial(self.register)
+        update_wrapper(directive, self.define_fn)
+        app.add_directive(self.name, directive)
+
+        def _include() -> None:
+            if not self.seen or not self.requires:
+                return
+
+            seen = app._aggressive_import_cache
+            for path in self.requires:
+                if path in seen:
+                    continue
+                app.include(path)
+
+        app.action(self.name, _include)
+
+
+def directive(
+    directive_fn: t.Optional[t.Callable[..., t.Any]] = None,
+    *,
+    name: str,
+    requires: t.List[str],
+):
+    def _directive(directive_fn: t.Callable[..., t.Any]):
+        ob = Directive(directive_fn, name=name, requires=requires)
+        update_wrapper(ob, directive_fn)
+        return ob
+
+    return _directive
+
 
 _has_subparser = False
 
@@ -34,122 +92,47 @@ def add_subcommand(app: App) -> None:
     app.add_directive("add_subcommand", _add_subcommand)
 
 
-def define_cli(app: App) -> None:
-    name = "define_cli"
-    seen = False
-
-    def _register_cli(app: App, kit: str) -> AnyFunction:
-        nonlocal seen
-        if not seen:
-            seen = True
-
-        def _register(fn: AnyFunction) -> AnyFunction:
-            app.registry.generators[kit].append(fn)
-            app.registry._task_list.append(fn.__name__)
-            return fn
-
-        return _register
-
-    app.add_directive(name, _register_cli)
-
-    def _include() -> None:
-        nonlocal seen
-        if seen:
-            app.include("egoist.generators.clikit")
-
-    # for conflict check
-    app.action(name, _include)
+@directive(name="define_cli", requires=["egoist.generators.clikit"])
+def define_cli(app: App, fn: AnyFunction, kit: str) -> AnyFunction:
+    app.registry.generators[kit].append(fn)
+    app.registry._task_list.append(fn.__name__)
+    return fn
 
 
-def define_struct_set(app: App) -> None:
-    name = "define_struct_set"
-    seen = False
-
-    def _register_struct_set(app: App, kit: str) -> AnyFunction:
-        nonlocal seen
-        if not seen:
-            seen = True
-
-        def _register(fn: AnyFunction) -> AnyFunction:
-            app.registry.generators[kit].append(fn)
-            app.registry._task_list.append(fn.__name__)
-            return fn
-
-        return _register
-
-    app.add_directive(name, _register_struct_set)
-
-    def _include() -> None:
-        nonlocal seen
-        if seen:
-            app.include("egoist.generators.structkit")
-
-    # for conflict check
-    app.action(name, _include)
+@directive(name="define_struct_set", requires=["egoist.generators.structkit"])
+def define_struct_set(app: App, fn: AnyFunction, kit: str) -> AnyFunction:
+    app.registry.generators[kit].append(fn)
+    app.registry._task_list.append(fn.__name__)
+    return fn
 
 
-def define_file(app: App) -> None:
-    name = "define_file"
-    seen = False
-
-    def _register_file(
-        app: App, kit: str, *, rename: t.Optional[str] = None, suffix: str = "",
-    ) -> AnyFunction:
-        nonlocal seen
-        if not seen:
-            seen = True
-
-        def _register(fn: AnyFunction) -> AnyFunction:
-            if rename is not None:
-                fn._rename = rename  # type: ignore
-            elif suffix:
-                fn._rename = f"{fn.__name__}{suffix}"  # type: ignore
-            app.registry.generators[kit].append(fn)
-            app.registry._task_list.append(fn.__name__)
-            return fn
-
-        return _register
-
-    app.add_directive(name, _register_file)
-
-    def _include() -> None:
-        nonlocal seen
-        if seen:
-            app.include("egoist.generators.filekit")
-
-    # for conflict check
-    app.action(name, _include)
+@directive(name="define_file", requires=["egoist.generators.filekit"])
+def define_file(
+    app: App,
+    fn: AnyFunction,
+    kit: str,
+    *,
+    rename: t.Optional[str] = None,
+    suffix: str = "",
+) -> AnyFunction:
+    if rename is not None:
+        fn._rename = rename  # type: ignore
+    elif suffix:
+        fn._rename = f"{fn.__name__}{suffix}"  # type: ignore
+    app.registry.generators[kit].append(fn)
+    app.registry._task_list.append(fn.__name__)
+    return fn
 
 
-def define_dir(app: App) -> None:
-    name = "define_dir"
-    seen = False
-
-    def _register_dir(
-        app: App, kit: str, *, rename: t.Optional[str] = None
-    ) -> AnyFunction:
-        nonlocal seen
-        if not seen:
-            seen = True
-
-        def _register(fn: AnyFunction) -> AnyFunction:
-            if rename is not None:
-                fn._rename = rename  # type: ignore
-            app.registry.generators[kit].append(fn)
-            app.registry._task_list.append(fn.__name__)
-            return fn
-
-        return _register
-
-    app.add_directive(name, _register_dir)
-
-    def _include() -> None:
-        nonlocal seen
-        if seen:
-            app.include("egoist.generators.dirkit")
-
-    # for conflict check
-    app.action(name, _include)
+@directive(name="define_dir", requires=["egoist.generators.dirkit"])
+def define_dir(
+    app: App, fn: AnyFunction, kit: str, *, rename: t.Optional[str] = None,
+) -> AnyFunction:
+    if rename is not None:
+        fn._rename = rename  # type: ignore
+    app.registry.generators[kit].append(fn)
+    app.registry._task_list.append(fn.__name__)
+    return fn
 
 
 def shared(app: App) -> None:
