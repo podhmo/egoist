fef901f8f381a30844478863292b8f7448383f97
podhmo <ababjam61+github@gmail.com>
2020-05-24 02:36:43 +0900

    chore: main.go

--- examples/todo-app/definitions.py
+++ examples/todo-app/definitions.py
@@ -7,12 +7,41 @@ app.include("egoist.directives.define_cli")
 
 
 @app.define_cli("egoist.generators.clikit:walk")
-def todo(*, name: str) -> None:
+def todo() -> None:
     """todo message"""
     from egoist.generators.clikit import runtime, clikit
 
-    with runtime.generate(clikit):
-        runtime.printf("todo %s\n", name)
+    with runtime.generate(clikit) as m:
+        todo_pkg = m.import_("m/todo")
+        fmt_pkg = m.import_("fmt")
+
+        err = m.symbol("err")
+        x = m.symbol("x")
+        args = runtime.get_cli_rest_args()
+
+        # if err := todo.Load(); err != nil {
+        # 	return err
+        # }
+        with m.if_(f"{err} := {todo_pkg.Load()}; {err} != nil"):
+            m.return_(err)
+
+        # for _, x := range opt.Args {
+        # 	todo.Add(x)
+        # }
+        with m.for_(f"_, {x} := range {args}"):
+            m.stmt(todo_pkg.Add(x))
+
+        # for _, x := range todo.List() {
+        # 	fmt.Println(x)
+        # }
+        with m.for_(f"_, {x} := range {todo_pkg.List()}"):
+            m.stmt(fmt_pkg.Println(x))
+
+        # if err := todo.Save(); err != nil {
+        # 	return err
+        # }
+        with m.if_(f"{err} := {todo_pkg.Save()}; {err} != nil"):
+            m.return_(err)
 
 
 if __name__ == "__main__":
