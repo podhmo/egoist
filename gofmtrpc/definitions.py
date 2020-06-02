19d07fb69275872acd18c8fae836e6e35cce0f7f
podhmo <ababjam61+github@gmail.com>
2020-06-03 08:51:37 +0900

    chore: add definitions.py and gen

---
+++ gofmtrpc/definitions.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+import typing as t
+from egoist.app import create_app, SettingsDict, parse_args
+from egoist.go.types import GoError
+
+
+settings: SettingsDict = {"rootdir": "cmd/", "here": __file__}
+app = create_app(settings)
+
+app.include("egoist.directives.define_cli")
+
+
+@app.define_cli("egoist.generators.clikit:walk")
+def gofmtrpc(*, addr: str = ":9999") -> GoError:
+    """gofmtrpc message"""
+    from egoist.generators.clikit import runtime, clikit
+
+    with runtime.generate(clikit) as m:
+        gofmtrpc_pkg = m.import_("github.com/podhmo/egoist/gofmtrpc")
+        m.return_(gofmtrpc_pkg.Run(addr))
+
+
+if __name__ == "__main__":
+    for argv in parse_args(sep="-"):
+        app.run(argv)
