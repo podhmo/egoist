2779592ca1d182830e1509ab4f629c2edae662ae
podhmo <ababjam61+github@gmail.com>
2020-06-06 21:01:21 +0900

    Server process (#120)
    * feat: ext serverprocess
    
    * fix: skip if dry run
    
    * fix: fix for typing

---
+++ egoist/ext/serverprocess/components/httpclient.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+import typing as t
+from egoist.app import App
+
+if t.TYPE_CHECKING:
+    from requests import Session
+    from egoist.internal.fake import Symbol
+
+NAME = __name__
+
+
+def get_http_client() -> Session:
+    from egoist.runtime import get_component_factory
+
+    return get_component_factory(NAME)()  # type: ignore
+
+
+def _create_http_client() -> Session:
+    import requests
+
+    return requests.Session()
+
+
+def _create_fake_http_client() -> Symbol:
+    from egoist.internal import fake
+
+    return fake.Symbol(__name__)
+
+
+def includeme(app: App) -> None:
+    app.register_factory(NAME, _create_http_client)
+    app.register_dryurn_factory(NAME, _create_fake_http_client)
---
+++ egoist/ext/serverprocess/lazyparams.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+import typing as t
+import typing_extensions as tx
+import logging
+from egoist.app import App
+
+logger = logging.getLogger(__name__)
+
+# for add_server_process()'s params parameter
+# using these functions, like below
+#
+# `app.add_server_process("xxxrpc --port {port}", params={"port": find_free_port})`
+
+
+class LazyParam(tx.Protocol):
+    def __call__(self, app: App) -> str:
+        ...
+
+
+def find_free_port(app: App) -> str:
+    return str(_find_free_port(host="0.0.0.0"))
+
+
+def create_sentinel_file(app: App) -> str:
+    return _create_sentinel_file(suffix=".sentinel")
+
+
+def _find_free_port(host: str = "0.0.0.0") -> int:
+    import socket
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind((host, 0))
+        sock.listen(1)
+        name, port = sock.getsockname()  # type: t.Tuple[str, int]
+    return port
+
+
+def _create_sentinel_file(suffix: t.Optional[str] = None) -> str:
+    import tempfile
+
+    fd, sentinel = tempfile.mkstemp(suffix=suffix)
+    logger.debug("create sentinel %s", sentinel)
+    return sentinel
